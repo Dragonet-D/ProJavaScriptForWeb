617411cd76beac8e6049d7af02ec5523a377b3f5
dragonet-d <Dragonet_D@163.com>
2018-02-04 00:37:07 +0800

    aaa

--- page3/index.js
+++ page3/index.js
@@ -1,2 +1,17 @@
+// 数值范围
 console.log(null == undefined);
 console.log(null === undefined);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+let result = Number.MAX_VALUE + Number.MAX_VALUE;
+console.log(result);
+console.log(isFinite(result));
+
+// NAN
+
+console.log(NaN === NaN);
+
+//  数值转换
+console.log(Number(undefined)); // NAN
+console.log(Number('0001'));
+console.log(Number('hello world'));
